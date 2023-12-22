6d8325f471a5e3314d61a70c82b7766af33a1701
Elisa ROGGE <elisa.rogge59@gmail.com>
2023-12-22 11:47:39 +0100

    newbills test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,32 +1,62 @@
-import NewBill from '../containers/NewBill';
-import { fireEvent, screen } from '@testing-library/dom';
+import { localStorageMock } from "../__mocks__/localStorage";
+import NewBill from "../containers/NewBill";
+import { fireEvent, screen } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI";
+import router from "../app/Router.js";
+import { ROUTES } from "../constants/routes.js";
+import mockStore from '../__mocks__/store.js'
 
-describe('NewBill', () => {
-  it('should handle file change', async () => {
-    const wrongFormatFile = new File(["hello"], "hello.txt", { type: "document/txt" })
-    const mockGetElementById = jest.fn()
-    const mockErrorObj = {};
-    mockGetElementById.mockReturnValue(mockErrorObj)
-    const documentMock = {
-      querySelector: (s) => {
-        console.log('querySelector with ', s)
-        if (s === 'input[data-testid="file"]') {
-          return {
-            files: [wrongFormatFile],
-            addEventListener: () => true,
-          }
-        } else {
-          return { addEventListener: () => true }
-        }
-      },
-      getElementById: mockGetElementById
-    }        
-    const objInstance = new NewBill({
-      document: documentMock,
-      onNavigate: {},
-      store: {},            
-      localStorage: {}
-    })
-    objInstance.handleChangeFile({ preventDefault: () => true, target: {value: 'hello.txt'} })
+const onNavigate = (pathname) => {
+  window.location.innerHTML = ROUTES({ pathname });
+};
+
+beforeEach(() => {
+  Object.defineProperty(window, "localStorage", { value: localStorageMock });
+  window.localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "employe@test.ltd" }));
+  const root = document.createElement("div");
+  root.setAttribute("id", "root");
+  document.body.append(root);
+  router();
+});
+
+describe("NewBill", () => {
+  it("should not handle file change", async () => {
+    document.body.innerHTML = NewBillUI();
+    const newBill = new NewBill({
+      document: document,
+      onNavigate: onNavigate,
+      store: mockStore,
+      localStorage: window.localStorage,
+    });
+    newBill.handleChangeFile({ preventDefault: () => true, target: {value: 'hello.txt'} })
+
+  });
+  it("should handle file change", async () => {
+    document.body.innerHTML = NewBillUI();
+    const newBill = new NewBill({
+      document: document,
+      onNavigate: onNavigate,
+      store: mockStore,
+      localStorage: window.localStorage,
+    });
+    newBill.handleChangeFile({ preventDefault: () => true, target: {value: 'hello.png'} })
+
+  });
+  it("should handle submit", async () => {
+    document.body.innerHTML = NewBillUI();
+    const newBill = new NewBill({
+      document: document,
+      onNavigate: onNavigate,
+      store: mockStore,
+      localStorage: window.localStorage,
+    });
+    const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
+
+    const form = screen.getByTestId("form-new-bill")
+    form.addEventListener("submit", handleSubmit);
+    fireEvent.submit(form);
+    expect(handleSubmit).toHaveBeenCalled();
+    expect(form).toBeDefined();
+
   });
 });
